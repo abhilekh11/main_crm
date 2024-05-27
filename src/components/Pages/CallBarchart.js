9692620b0d7a269cb4822f285e27ddfa455b30ef
bittusing <abhilekhk6@gmail.com>
2024-05-27 18:23:56 +0530

    uplode

--- src/components/Pages/CallBarchart.js
+++ src/components/Pages/CallBarchart.js
@@ -28,7 +28,7 @@ const data = {
     labels: ['Amit', 'Dinesh', 'Ramesh', 'Anil', 'Shubhash', 'Chirag', 'Umesh'],
     datasets: [
         {
-            label: 'Incomming Call',
+            label: 'Incoming Call',
             data: [65, 59, 80, 81, 56, 55, 40],
             backgroundColor: 'rgba(75, 192, 192, 0.6)',
             borderColor: 'rgba(75, 192, 192, 1)',
